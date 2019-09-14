a0202d93fda89680737e986e5e23ee62bbe6e495
Colby Reed <43652809+ColbyReed@users.noreply.github.com>
2019-09-14 16:02:18 -0500

    Initial commit

---
+++ app/src/main/java/com/mobile/calculator/controller/CalculatorActivity.java
@@ -0,0 +1,14 @@
+package com.mobile.calculator.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mobile.calculator.R;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculator);
+    }
+}
---
+++ app/src/main/java/com/mobile/calculator/model/Buttons.java
@@ -0,0 +1,7 @@
+package com.mobile.calculator.model;
+
+public class Buttons {
+
+
+
+}
