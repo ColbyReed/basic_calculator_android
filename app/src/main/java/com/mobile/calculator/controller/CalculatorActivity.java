e791164b02d01d3ffda71ef3e43aafaabd0da1dc
Colby Reed <43652809+ColbyReed@users.noreply.github.com>
2019-09-15 02:58:05 -0500

    Added 1 button functionality Added TextView for number input

--- app/src/main/java/com/mobile/calculator/controller/CalculatorActivity.java
+++ app/src/main/java/com/mobile/calculator/controller/CalculatorActivity.java
@@ -2,13 +2,73 @@ package com.mobile.calculator.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.mobile.calculator.R;
 
 public class CalculatorActivity extends AppCompatActivity {
+
+    Button btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnZero,
+            btnAddition, btnSubtract, btnDivide, btnMultiply, btnEqual, btnClear, btnBackspace, btnPosNeg, btnDecimal;
+    TextView txtCalculations;
+    Double valueOne, valueTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator);
+
+        btnOne = findViewById(R.id.btnOne);
+        btnTwo = findViewById(R.id.btnTwo);
+        btnThree = findViewById(R.id.btnThree);
+        btnFour = findViewById(R.id.btnFour);
+        btnFive = findViewById(R.id.btnFive);
+        btnSix = findViewById(R.id.btnSix);
+        btnSeven = findViewById(R.id.btnSeven);
+        btnEight = findViewById(R.id.btnEight);
+        btnNine = findViewById(R.id.btnNine);
+        btnZero = findViewById(R.id.btnZero);
+
+        btnAddition = findViewById(R.id.btnAddition);
+        btnSubtract = findViewById(R.id.btnSubtract);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnEqual = findViewById(R.id.btnEqual);
+
+        btnClear = findViewById(R.id.btnClear);
+        btnBackspace = findViewById(R.id.btnBackspace);
+        btnPosNeg = findViewById(R.id.btnPosNeg);
+        btnDecimal = findViewById(R.id.btnDecimal);
+
+        txtCalculations = findViewById(R.id.txtCalculations);
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCalculations.setText("");
+            }
+        });
+
+        btnBackspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number = txtCalculations.getText().toString();
+                if(number.length() > 0){
+                    number = number.substring(0, number.length() - 1);
+                }
+                txtCalculations.setText(number);
+            }
+        });
+
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtCalculations.setText(txtCalculations.getText() + "1");
+            }
+        });
+
     }
 }
